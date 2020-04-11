6d3088de6bd590c8494279c0a1849d0b3bd81558
Okechukwu Ochia <emmirald@gmail.com>
2020-04-11 05:56:22 -0600

    Added an event and an event handler for deleting the Increment button

--- shopping-cart/src/components/counter.jsx
+++ shopping-cart/src/components/counter.jsx
@@ -6,7 +6,7 @@ class Counter extends Component {
     this.handleIncrement = this.handleIncrement.bind(this); // bind onclick handler to object of counter
   }*/
   state = {
-    value: this.props.value, // initialize the count property by setting it to the value
+    value: this.props.counter.value, // initialize the count property by setting it to the value
     tags: ["tag1", "tag2", "tag3"],
   };
 
@@ -15,7 +15,7 @@ class Counter extends Component {
     this.setState({ value: this.state.value + 1 });
   };
 
-  renderTags() {
+  /* renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     else
       return (
@@ -25,7 +25,7 @@ class Counter extends Component {
           ))}
         </ul>
       );
-  }
+  } */
 
   render() {
     //console.log("props", this.props); log all the properties or attributes of Counter to the console
@@ -38,8 +38,13 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
         {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
       </div>
     );
   }
--- shopping-cart/src/components/counters.jsx
+++ shopping-cart/src/components/counters.jsx
@@ -3,7 +3,7 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-    // add a counters key whose value is an array of objects
+    // add a counters key whose value is an array of counter objects
     counters: [
       { id: 1, value: 3 },
       { id: 2, value: 0 },
@@ -11,13 +11,24 @@ class Counters extends Component {
       { id: 4, value: 1 },
     ],
   };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId); //decalre a counter constant and filter out the id from the counter
+    this.setState({ counters: counters }); // update the counter array in the state to the new counter constant
+    //this.setState({ counters}) also works because the key and value names are the same.
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((
           counter // map each counter to a Counter component and set its properties
         ) => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     ); // div to wrap all counters
