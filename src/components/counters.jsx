2bfa2fa23d4281675f58da2bbb74c862803ae42f
Okechukwu Ochia <emmirald@gmail.com>
2020-04-12 03:39:39 -0600

    Added a Reset button to reset all counter items to zero

---
+++ src/components/counters.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    // add a counters key whose value is an array of counter objects
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId); //decalre a counter constant and filter out the id from the counter
+    this.setState({ counters }); // update the counter array in the state to the new counter constant
+    //this.setState({ counters}) also works because the key and value names are the same.
+  };
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2 "
+        >
+          Reset
+        </button>
+        {this.state.counters.map((
+          counter // map each counter to a Counter component and set its properties
+        ) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    ); // div to wrap all counters
+  }
+}
+
+export default Counters;
