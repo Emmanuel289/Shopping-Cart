54eb1fd5f0021ec713e837d6c1a948af22d25adb
Okechukwu Ochia <emmirald@gmail.com>
2020-04-12 23:54:25 -0600

    Lifted all states in Counters to App by referencing its event handlers

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,39 +2,7 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    // add a counters key whose value is an array of counter objects
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId); //decalre a counter constant and filter out the id from the counter
-    this.setState({ counters }); // update the counter array in the state to the new counter constant
-    //this.setState({ counters}) also works because the key and value names are the same.
-  };
-
   render() {
-    console.log("Counters-Rendered");
     return (
       <div>
         <button
@@ -43,7 +11,9 @@ class Counters extends Component {
         >
           Reset
         </button>
-        {this.props.counters.map((counter) => (
+        {this.props.counters.map((
+          counter // map each counter to a Counter component and set its properties
+        ) => (
           <Counter
             key={counter.id}
             onDelete={this.props.onDelete}
